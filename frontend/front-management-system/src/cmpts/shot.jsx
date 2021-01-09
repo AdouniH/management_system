5e3c82d26c4b8e52928d16e0f8c17a97cefd78e3
Houssem ADOUNI <houssemadouni11@gmail.com>
2021-01-09 21:55:21 +0100

    save changes

--- frontend/front-management-system/src/cmpts/shot.jsx
+++ frontend/front-management-system/src/cmpts/shot.jsx
@@ -7,6 +7,12 @@ import {CopyToClipboard} from 'react-copy-to-clipboard';
 import './style/shot.css'
 import {REACT_APP_BACKEND_URL} from '../config_urls.js'
 import load from './style/load.gif'
+import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
 
 
 function Teams(props) {
@@ -30,7 +36,7 @@ function Teams(props) {
       <div>
           <p className="info">
               En confirmant la réunion vous recevez un mail de confirmation
-              avec le lien de la reunion TEAMS
+              avec plus de detail
           </p>
       </div>
   );
@@ -41,8 +47,8 @@ function Skype() {
   return (
       <div>
           <p className="info">
-              En confirmant la réunion vous recevez un mail de confirmation
-              avec le lien de la reunion SKYPE
+          En confirmant la réunion vous recevez un mail de confirmation
+          avec plus de detail
           </p>
       </div>
   );
@@ -50,29 +56,42 @@ function Skype() {
 
 function Phone() {
   return (
-      <div>
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
-
-      <p className="infophone">En confirmant la réunion vous recevez un mail contenant mon numéro de télephone</p>
-      <div className="center">
-          <input className='phone' type='text' value='0758553568'/>
-          <CopyToClipboard text='0758553568'
-              onCopy={() => console.log("copied")}>
-              <button class="btn" type="button"><i class="fa fa-copy"></i></button>
-          </CopyToClipboard>
-      </div>
-      </div>
+    <div>
+        <p className="info">
+        En confirmant la réunion vous recevez un mail de confirmation
+        avec plus de detail
+        </p>
+    </div>
   );
 }
 
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+  },
+  text_input: {
+    marginTop: 20,
+    width: '100%',
+  },
+  select_input: {
+    marginTop: 20,
+    width: '100%'
+  },
+  label: {
+    backroundColor: 'white',
+    padding: 5,
+    zIndex: 5
+  }
+}));
+
 export default function ShotPicker(props) {
+  const classes = useStyles();
   const history = useHistory();
   let {id} = useParams()
   const token = useSelector(state => state.auth.token);
 
-  const [name, setName] = useState(null);
-  const [email, setEmail] = useState(null);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
   const [duree, setDuree] = useState('30');
   const [toolvalue, setToolValue] = useState('teams');
   const [link, setLink] = useState(null);
@@ -81,7 +100,6 @@ export default function ShotPicker(props) {
   const [hour, setHour] = useState('');
   const [submitLoading, setSubmitLoading] = useState(false);
   const [initLoading, setInitLoading] = useState(false);
-
 
   const toolChanged = (event) => {
       setToolValue(event.target.value);
@@ -154,63 +172,70 @@ export default function ShotPicker(props) {
                   <img className="loadimg" src={load}></img>
                                     :
                   <div className='title'>
-                      <h4> Veuillez confirmer cet entretien du </h4>
-                      <h4> {date} à {hour}</h4>
+                      <h5> Veuillez confirmer cet entretien du </h5>
+                      <h5> {date} à {hour}</h5>
                   </div>
               }
-              <div className='rowform'>
-                  <p>Votre Nom</p>
-                  <input className='finput'
-                    type='text'
-                    name='name'
+              <TextField
+                    className={classes.text_input}
+                    label="Votre nom"
                     value={name}
                     onChange={(event) => setName(event.target.value)}
-                  />
-              </div>
-              <div className='rowform'>
-                  <p>Votre Email</p>
-                  <input className='finput'
+              />
+              <TextField
+                    className={classes.text_input}
+                    label="Votre email"
                     type='email'
-                    name='email'
                     value={email}
                     required
                     onChange={(event) => setEmail(event.target.value)}
-                  />
-              </div>
-              <br/>
-              <div className='rowform'>
-                  <p>Durée de l'entretien</p>
-                  <select onChange={(event) => {setDuree(event.target.value)}}>
-                      <option value="15">15 minutes</option>
-                      <option value="30" selected>30 minutes</option>
-                      <option value="60">1 heure</option>
-                  </select>
-              </div>
-              <br/>
-              <div className='rowform'>
-                  <p>Entretien via:</p>
-                  <select onChange={toolChanged}>
-                      <option value="teams">Microsoft Teams</option>
-                      <option value="skype">Skype</option>
-                      <option value="phone">Appel télephonique</option>
-                  </select>
-              </div>
+                    />
+
+              <FormControl className={classes.select_input}>
+                <InputLabel id="demo-simple-select-label">Durée de l'entretien</InputLabel>
+                <Select
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  value={duree}
+                  onChange={(event) => {setDuree(event.target.value)}}
+                >
+                  <MenuItem value={'15'}>15 minutes</MenuItem>
+                  <MenuItem value={'30'}>30 minutes</MenuItem>
+                  <MenuItem value={'60'}>1 heure</MenuItem>
+                </Select>
+              </FormControl>
+              <FormControl className={classes.select_input}>
+                <InputLabel id="demo-simple-select-label">Entretien via</InputLabel>
+                <Select
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  value={toolvalue}
+                  onChange={toolChanged}
+                >
+                  <MenuItem value={"teams"}>Microsoft Teams</MenuItem>
+                  <MenuItem value={"skype"}>Skype</MenuItem>
+                  <MenuItem value={"phone"}>Appel télephonique</MenuItem>
+                </Select>
+              </FormControl>
+              <TextField
+                className={classes.text_input}
+                id="outlined-multiline-static"
+                label="Commentaire (optionnel)"
+                multiline
+                rows={4}
+                defaultValue="Default Value"
+                variant="outlined"
+                value={tarea}
+                onChange={(event) => {setTarea(event.target.value)}}
+              />
               <div className='rowform'>
                   {tool}
               </div>
-              <div className='rowform'>
-                  <p> Commentaire (facultatif)</p>
-                  <textarea
-                      id="comment"
-                      name="comment"
-                      value={tarea}
-                      onChange={(event) => {setTarea(event.target.value)}}>
-                  </textarea>
-              </div>
+
               <br/>
               <div className='rowforma'>
                   {submitLoading ? <img className="loadimg" src={load}></img> : null}
-                  <input className='submitf' type='submit' value='confirmer votre rendez vous'/>
+                  <input className='submitf' type='submit' value='confirmer'/>
                   {submitLoading ? <img className="loadimg" src={load}></img> : null}
               </div>
 
