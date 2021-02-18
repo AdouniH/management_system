12a50f574a90887aca9bdafe08492880041d9a1b
Houssem ADOUNI <houssemadouni11@gmail.com>
2021-02-18 21:28:40 +0100

    save changes

--- frontend/front-management-system/src/pages/dataPage/datapage.jsx
+++ frontend/front-management-system/src/pages/dataPage/datapage.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
 import NavBar from './layouts/navbar.jsx'
 import DataLayout from './layouts/datalayout.jsx'
+import './style/data.css'
 
 
 function DataPage() {
-
   return (
     <div>
       <NavBar/>
--- frontend/front-management-system/src/pages/dataPage/layouts/navbar.jsx
+++ frontend/front-management-system/src/pages/dataPage/layouts/navbar.jsx
@@ -1,13 +1,42 @@
 import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import '../style/data.css'
+import { useDispatch } from 'react-redux'
+import { disconnect } from '../../../redux'
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
 
 
 function NavBar() {
-
+  const classes = useStyles();
+  const dispatch = useDispatch()
   return (
     <div>
-      <h1>Navbar</h1>
+      <AppBar position="static">
+        <Toolbar className="tbar">
+          <Button color="inherit" onClick={() => {dispatch(disconnect())}}>Se déconnecter</Button>
+        </Toolbar>
+      </AppBar>
     </div>
   );
 }
+
 
 export default NavBar;
